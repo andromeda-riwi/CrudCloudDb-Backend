da157a6f987aea202db5a458ad6ed68869027665
San1000-Ark <hacksan1000@gmail.com>
2025-11-07 18:29:51 -0500

    Add the resolve conflicts

--- src/CCD.Infrastructure/Data/ApplicationDbContext.cs
+++ src/CCD.Infrastructure/Data/ApplicationDbContext.cs
@@ -53,20 +53,13 @@ public class ApplicationDbContext : DbContext
         });
         
         modelBuilder.Entity<Plan>()
-<<<<<<< HEAD
+
             .HasMany(p => p.Users)      
             .WithOne(u => u.Plan)       
             .HasForeignKey(u => u.PlanId) 
             .IsRequired();              
         
-=======
-            .HasMany(p => p.Users)
-            .WithOne(u => u.Plan)
-            .HasForeignKey(u => u.PlanId)
-            .IsRequired();
 
-        // Relación Uno-a-Muchos: Un Usuario tiene muchas DatabaseInstances
->>>>>>> 8f2f500fc835ffd00a9f47e740f38be94163a140
         modelBuilder.Entity<User>()
             .HasMany(u => u.DatabaseInstances)
             .WithOne(d => d.User)
