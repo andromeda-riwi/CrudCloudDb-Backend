f27e452d4f1f44b20b89fc1170100ff2bf08f39c
Steven Cardona <stevencardona2001@gmail.com>
2025-11-07 17:01:20 -0500

    Revert "Feature/santiagorestrepoarismendy"

--- src/CCD.Api/Dtos/CreatePreferenceResponseDto.cs
+++
@@ -1,7 +0,0 @@
-namespace CCD.Api.Dtos;
-
-public class CreatePreferenceResponseDto
-{
-    public string PreferenceId { get; set; } = string.Empty;
-    public string InitPoint { get; set; } = string.Empty;  // URL de pago
-}
